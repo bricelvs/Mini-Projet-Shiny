f7378263ebef95645c294327617df2213a8fb56a
bricelvs <blevasseur@etu.isara.fr>
2021-05-03 15:18:47 +0200

    questions maj plus supp des première selection ui

--- Quiz/ui.R
+++ Quiz/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -17,46 +8,47 @@ shinyUI(fluidPage(
     
     # Sidebar with a slider input for number of bins
     
-    radioButtons(inputId = "idRadio1", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio1", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio2", label = "Quel est la capitale de la France", selected = 3,
+    radioButtons(inputId = "idRadio2", label = "Quel est la capitale de la France", selected = F,
                  choices =c("Paris" = 1, "Berlin" = 2, "Monaco" = 3)),
-    radioButtons(inputId = "idRadio3", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio3", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio4", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio4", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio5", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio5", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio6", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio6", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio7", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio7", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio8", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio8", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio9", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio9", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio10", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio10", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio11", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio11", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio12", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio12", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio13", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio13", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio14", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio14", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio15", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio15", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio16", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio16", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio17", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio17", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio18", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio18", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio19", label = "Select one", selected = 3,
+    radioButtons(inputId = "idRadio19", label = "Select one", selected = F,
                  choices =c("First" = 1, "Second" = 2, "Third" = 3)),
-    radioButtons(inputId = "idRadio20", label = "Select one", selected = 3,
-                 choices =c("First" = 1, "Second" = 2, "Third" = 3)),  # For the server input$idRadio is a "character"13))
+    radioButtons(inputId = "idRadio20", label = "Que veut dire ISARA ?", selected = F,
+                 choices =c("First" = 1, "Institut supérieur d'agriculture Rhône-Alpes" = 2, "Third" = 3)),  
+    # For the server input$idRadio is a "character"13))
     actionButton(inputId = "idActionButton", label = "Valider !",icon =icon("hand-spock-o"))
     
 ))
